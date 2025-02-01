d0146acaaba07250993ab65f001d43153f057437
unknown <notnotsky26u@gmaile.com>
2025-02-01 18:34:15 +0530

    idk just saving changesk

--- main.jsx
+++ main.jsx
@@ -1,25 +1,7 @@
-let calculatedResultNum = 0;
+console.log("hello working?");
+let answersLearned = 0;
 function App() {
   const { useState } = React;
-  // const [userInputValue, setUserInputValue] = useState("");
-  // const hanldeUserInputValueChange = (event) => {
-  //   setUserInputValue(event.target.value);
-  // };
-  // const handleUserInputSubmit = () => {
-  //   if (userInputValue.trim() === "") {
-  //     return "Enter something";
-  //   } else if (isNaN(userInputValue) === false) {
-  //     calculatedResultNum = userInputValue / 2;
-  //     return "akash has earned " + calculatedResultNum + " magi packet";
-  //   } else if (isNaN(userInputValue) === true) {
-  //     return "enter a number";
-  //   } else {
-  //     return "Oops!. something went wrong try again(try reloading)";
-  //   }
-  // };
-  // const calculatePrice = () => {
-  //   return calculatedResultNum * 14;
-  // };
   return (
     <div
       className="h-screen bg-gray-100 flex flex-col justify-center align-middle items-center"
@@ -29,22 +11,18 @@ function App() {
         className="w-4/5 h-[75vh] outline flex flex-col bg-white rounded-xl justify-center items-center"
         id="card"
       >
-        {/* <div className="absolute top-1 mt-12" id="main"> */}
-
         <p className="text-2xl m-5" id="question1">
           how many answers did akash learn?
         </p>
+        <p className="m-5" id="answersLearnedOutput">
+          akash has currently learned{" "}
+          <span className="text-blue-500 text-xl">{}</span> answers
+        </p>
+
         <div
           className="flex flex-row justify-center items-center"
           id="inputDiv"
         >
-          {/* <input
-                type="text"
-                className="w-32 h-12 outline rounded-md shadow-xl m-12 text-center placeholder-gray-500 "
-                id="question"
-                onChange={hanldeUserInputValueChange}
-                placeholder="enter it here"
-              /> */}
           <div className="flex gap-5" id="Btns">
             <button
               className="outline w-auto  p-5 flex items-center text-xl hover:bg-blue-200 h-12 rounded-md shadow-xl transition  hover:bg-blue-100"
@@ -60,16 +38,8 @@ function App() {
             </button>
           </div>
         </div>
-        {/* <p className="text-xl" id="output">
-          {handleUserInputSubmit()}
-        </p> */}
-
-        {/* <p className="m-5 text-xl" id="priceOutput">
-          estimated price: {calculatePrice()}
-        </p> */}
       </div>
     </div>
-    //  </div>
   );
 }
 
