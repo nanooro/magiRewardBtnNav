56e48ea949c2a8e2fda9d15f7ada8b226e40d891
unknown <notnotsky26u@gmaile.com>
2025-02-01 19:54:37 +0530

    added a bit of functionality to the btns and currently im jetlaged so i may not be thinking straight

--- main.jsx
+++ main.jsx
@@ -1,7 +1,13 @@
-console.log("hello working?");
-let answersLearned = 0;
+let answersLearnedCountRef = 1;
 function App() {
+  const incrementAnswersLearned = () => {
+    setAnswersLearned(answersLearnedCountRef++);
+  };
+  const decrementAnswersLearned = () => {
+    setAnswersLearned(answersLearnedCountRef--);
+  };
   const { useState } = React;
+  const [answersLearned, setAnswersLearned] = useState("");
   return (
     <div
       className="h-screen bg-gray-100 flex flex-col justify-center align-middle items-center"
@@ -11,12 +17,13 @@ function App() {
         className="w-4/5 h-[75vh] outline flex flex-col bg-white rounded-xl justify-center items-center"
         id="card"
       >
-        <p className="text-2xl m-5" id="question1">
+        <p className="text-4xl -mt-12 m-5" id="question1">
           how many answers did akash learn?
         </p>
-        <p className="m-5" id="answersLearnedOutput">
+        <p className="m-5 text-xl" id="answersLearnedOutput">
           akash has currently learned{" "}
-          <span className="text-blue-500 text-xl">{}</span> answers
+          <span className="text-blue-500 text-xl">{answersLearned}</span>{" "}
+          answers
         </p>
 
         <div
@@ -26,13 +33,15 @@ function App() {
           <div className="flex gap-5" id="Btns">
             <button
               className="outline w-auto  p-5 flex items-center text-xl hover:bg-blue-200 h-12 rounded-md shadow-xl transition  hover:bg-blue-100"
-              id="calculate"
+              id="incrementBtn"
+              onClick={incrementAnswersLearned}
             >
               +1
             </button>
             <button
               className="outline w-auto p-5 flex items-center text-xl hover:bg-red-200 h-12 rounded-md shadow-xl transition  hover:bg-blue-100"
-              id="calculate"
+              id="decrementBtn"
+              onClick={decrementAnswersLearned}
             >
               -1
             </button>
